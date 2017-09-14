5016c5d26858eecd4749d7f281df16877c9a7d01
SunnyLy <450512747@qq.com>
2017-09-14 23:05:51 +0800

    曲线图表完成。。。

--- app/src/main/java/chart/muke/com/mukechart/basicchart/CurveChartActivity.java
+++ app/src/main/java/chart/muke/com/mukechart/basicchart/CurveChartActivity.java
@@ -1,11 +1,21 @@
 package chart.muke.com.mukechart.basicchart;
 
 import android.graphics.Color;
+import android.view.DragEvent;
 import android.view.View;
 
 import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.Description;
 import com.github.mikephil.charting.components.XAxis;
 import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.formatter.IFillFormatter;
+import com.github.mikephil.charting.interfaces.dataprovider.LineDataProvider;
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
+
+import java.util.ArrayList;
 
 import chart.muke.com.mukechart.R;
 import chart.muke.com.mukechart.base.MukeBaseActivity;
@@ -27,8 +37,66 @@ public class CurveChartActivity extends MukeBaseActivity {
     protected void initParams() {
         setTitle(R.string.title_curve_chart);
 
-        //初始化直线图相关参数
+        //1.初始化直线图相关参数
         initLineChart();
+        //2.获取图表数据
+        setChartData(45,100);
+        //3.进行绘制
+        mCurveChart.invalidate();
+
+    }
+
+    private void setChartData(int count,float range) {
+        ArrayList<Entry> yVals = new ArrayList<Entry>();
+
+        for (int i = 0; i < count; i++) {
+            float mult = (range + 1);
+            float val = (float) (Math.random() * mult) + 20;// + (float)
+            // ((mult *
+            // 0.1) / 10);
+            yVals.add(new Entry(i, val));
+        }
+
+        LineDataSet set1;
+
+        if (mCurveChart.getData() != null &&
+                mCurveChart.getData().getDataSetCount() > 0) {
+            set1 = (LineDataSet)mCurveChart.getData().getDataSetByIndex(0);
+            set1.setValues(yVals);
+            mCurveChart.getData().notifyDataChanged();
+            mCurveChart.notifyDataSetChanged();
+        } else {
+            // create a dataset and give it a type
+            set1 = new LineDataSet(yVals, "DataSet 1");
+
+            set1.setMode(LineDataSet.Mode.CUBIC_BEZIER);
+            set1.setCubicIntensity(0.2f);
+            //set1.setDrawFilled(true);
+            set1.setDrawCircles(false);
+            set1.setLineWidth(1.8f);
+            set1.setCircleRadius(4f);
+            set1.setCircleColor(Color.WHITE);
+            set1.setHighLightColor(Color.rgb(244, 117, 117));
+            set1.setColor(Color.WHITE);
+            set1.setFillColor(Color.WHITE);
+            set1.setFillAlpha(100);
+            set1.setDrawHorizontalHighlightIndicator(false);
+            set1.setFillFormatter(new IFillFormatter() {
+                @Override
+                public float getFillLinePosition(ILineDataSet dataSet, LineDataProvider dataProvider) {
+                    return -10;
+                }
+            });
+
+            // create a data object with the datasets
+            LineData data = new LineData(set1);
+          //  data.setValueTypeface(mTfLight);
+            data.setValueTextSize(9f);
+            data.setDrawValues(false);
+
+            // set data
+            mCurveChart.setData(data);
+        }
     }
 
     private void initLineChart() {
@@ -44,7 +112,12 @@ public class CurveChartActivity extends MukeBaseActivity {
         // if disabled, scaling can be done on x- and y-axis separately
         mCurveChart.setPinchZoom(false);
 
-        mCurveChart.setDrawGridBackground(true);
+        mCurveChart.setDrawGridBackground(false);//是否画网格背景
+
+        //设置附加信息
+        Description description = new Description();
+        description.setText("简单曲线图");
+        mCurveChart.setDescription(description);
 
         XAxis x = mCurveChart.getXAxis();
         x.setEnabled(false);//不使用常规的X轴
